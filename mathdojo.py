3843452a7e137567eaed881bfd3ac6936314be07
Cindy Cao <chcaohan@gmail.com>
2019-11-08 22:05:47 -0800

    second commit

---
+++ mathdojo.py
@@ -0,0 +1,16 @@
+class MathDojo:
+    def __init__(self):
+        self.result = 0
+    def add(self, num, *nums):
+        self.result += (num + sum(nums))
+        return self
+
+    def subtract(self, num, *nums):
+        self.result -= (num + sum(nums))
+        return self
+        
+test = MathDojo()
+
+x = test.add(2).add(2,5,1).subtract(3,2).result 
+
+print(x)
