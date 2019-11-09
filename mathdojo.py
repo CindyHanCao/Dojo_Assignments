4685b7417969acd554bcb6afef3124d32230c746
Cindy Cao <chcaohan@gmail.com>
2019-11-09 13:31:16 -0800

    add OOP folder

--- mathdojo.py
+++ mathdojo.py
@@ -2,13 +2,13 @@ class MathDojo:
     def __init__(self):
         self.result = 0
     def add(self, num, *nums):
-        self.result += (num + sum(nums))
+        self.result += (num + sum(nums))  #sum is a python built-in method.
         return self
 
     def subtract(self, num, *nums):
         self.result -= (num + sum(nums))
         return self
-        
+
 test = MathDojo()
 
 x = test.add(2).add(2,5,1).subtract(3,2).result 
